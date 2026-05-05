af097b1188af84091734e13dee34996e00748b0b
zamehani2 <zamehani2@users.noreply.github.com>
2026-05-05 13:29:53 +0400

    Add WPF remote item view models

---
+++ RemoteFileDialog.Wpf/ViewModels/RemoteListItemViewModel.cs
@@ -0,0 +1,66 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using RemoteFileDialog.Core.Enums;
+using RemoteFileDialog.Core.Models;
+
+namespace RemoteFileDialog.Wpf.ViewModels;
+
+/// <summary>
+/// Represents a remote item displayed in the right-side list.
+/// </summary>
+public sealed partial class RemoteListItemViewModel : ObservableObject
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RemoteListItemViewModel"/> class.
+    /// </summary>
+    /// <param name="remoteItem">The remote file or folder item.</param>
+    public RemoteListItemViewModel(RemoteItem remoteItem)
+    {
+        RemoteItem = remoteItem ?? throw new ArgumentNullException(nameof(remoteItem));
+    }
+
+    /// <summary>
+    /// Gets the underlying remote item.
+    /// </summary>
+    public RemoteItem RemoteItem { get; }
+
+    /// <summary>
+    /// Gets the item name.
+    /// </summary>
+    public string Name => RemoteItem.Name;
+
+    /// <summary>
+    /// Gets the full remote path.
+    /// </summary>
+    public string FullPath => RemoteItem.FullPath;
+
+    /// <summary>
+    /// Gets the item type.
+    /// </summary>
+    public RemoteItemType ItemType => RemoteItem.ItemType;
+
+    /// <summary>
+    /// Gets a value indicating whether this item is a directory.
+    /// </summary>
+    public bool IsDirectory => RemoteItem.IsDirectory;
+
+    /// <summary>
+    /// Gets a value indicating whether this item is a file.
+    /// </summary>
+    public bool IsFile => RemoteItem.IsFile;
+
+    /// <summary>
+    /// Gets the item size.
+    /// </summary>
+    public long? Size => RemoteItem.Size;
+
+    /// <summary>
+    /// Gets the item modified date.
+    /// </summary>
+    public DateTime? ModifiedDate => RemoteItem.ModifiedDate;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the item is selected.
+    /// </summary>
+    [ObservableProperty]
+    private bool _isSelected;
+}
---
+++ RemoteFileDialog.Wpf/ViewModels/RemoteTreeNodeViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace RemoteFileDialog.Wpf.ViewModels;
+
+/// <summary>
+/// Represents a folder node in the remote folder tree.
+/// </summary>
+public sealed partial class RemoteTreeNodeViewModel : ObservableObject
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="RemoteTreeNodeViewModel"/> class.
+    /// </summary>
+    /// <param name="name">The folder display name.</param>
+    /// <param name="fullPath">The full remote folder path.</param>
+    public RemoteTreeNodeViewModel(string name, string fullPath)
+    {
+        Name = name;
+        FullPath = fullPath;
+    }
+
+    /// <summary>
+    /// Gets the folder display name.
+    /// </summary>
+    public string Name { get; }
+
+    /// <summary>
+    /// Gets the full remote folder path.
+    /// </summary>
+    public string FullPath { get; }
+
+    /// <summary>
+    /// Gets the child folder nodes.
+    /// </summary>
+    public ObservableCollection<RemoteTreeNodeViewModel> Children { get; } = [];
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the node has already loaded children.
+    /// </summary>
+    [ObservableProperty]
+    private bool _isLoaded;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the node is expanded.
+    /// </summary>
+    [ObservableProperty]
+    private bool _isExpanded;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the node is selected.
+    /// </summary>
+    [ObservableProperty]
+    private bool _isSelected;
+}
